efe9719bc0379cf3ea5ae7d6fa48dd23d433b277
Silvio Roberto <sousasilvio441@gmail.com>
2026-06-05 10:08:13 -0300

    Configurações iniciais

---
+++ backend/src/main.ts
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module.js';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = `${process.env.PORT}`;
+
+  const config = new DocumentBuilder()
+    .setTitle('Rei do Palpite Documentation APIs')
+    .setDescription('Documentação das APIs do projeto Rei do Palpite')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = SwaggerModule.createDocument(app, config);
+
+  app.use(
+    '/scalar/documentation',
+    apiReference({
+      theme: 'default',
+      content: documentFactory,
+    }),
+  );
+  await app.listen(port);
+}
+bootstrap().catch((error) => console.log(error));
